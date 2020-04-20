bbaffa876133d4230684e34441d4f0235fb616ef
Ammar Faizi <ammarfaizi2@gmail.com>
2020-04-20 20:10:47 +0700

    Prepare config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+
+#ifndef GREENTEA_APP_CONFIG
+#define GREENTEA_APP_CONFIG
+
+#define BASEURL "http://127.0.0.1:8000"
+#define DB_HOST "127.0.0.1"
+#define DB_PORT 3306
+#define DB_USER "root"
+#define DB_PASS ""
+#define DB_NAME "greentea"
+
+#endif
