6408590432d8686e4e19b2f7481a786b3c157521
Ammar Faizi <ammarfaizi2@gmail.com>
2020-04-20 20:04:53 +0700

    Prepare module scripts

---
+++ src/greentea_php/greentea_php.c
@@ -0,0 +1,57 @@
+
+#define GREENTEA_MAIN
+#include "greentea_php.h"
+
+#ifdef COMPILE_DL_TEABOT7
+    #ifdef ZTS
+        ZEND_TSRMLS_CACHE_DEFINE()
+    #endif
+#endif
+
+ZEND_DECLARE_MODULE_GLOBALS(greentea);
+
+/**
+ * Init.
+ */
+static PHP_MINIT_FUNCTION(greentea)
+{
+    REGISTER_INI_ENTRIES();
+    return SUCCESS;
+}
+
+/**
+ * Shutdown.
+ */
+static PHP_MSHUTDOWN_FUNCTION(greentea)
+{
+    UNREGISTER_INI_ENTRIES();
+    return SUCCESS;
+}
+
+/**
+ * Global init.
+ */
+static PHP_GINIT_FUNCTION(greentea)
+{
+    #if defined(COMPILE_DL_ASTKIT) && defined(ZTS)
+        ZEND_TSRMLS_CACHE_UPDATE();
+    #endif
+}
+
+zend_module_entry greentea_module_entry = {
+    STANDARD_MODULE_HEADER,
+    "greentea",
+    NULL, /* functions */
+    PHP_MINIT(greentea),
+    PHP_MSHUTDOWN(greentea),
+    NULL, /* RINIT */
+    NULL, /* RSHUTDOWN */
+    NULL, /* MINFO */
+    TEABOT_VERSION,
+    PHP_MODULE_GLOBALS(greentea),
+    PHP_GINIT(greentea),
+    NULL, /* GSHUTDOWN */
+    NULL, /* RPOSTSHUTDOWN */
+    STANDARD_MODULE_PROPERTIES_EX
+};
+ZEND_GET_MODULE(greentea);
---
+++ src/greentea_php/greentea_php.h
@@ -0,0 +1,34 @@
+
+#ifndef GREENTEA_PHP_H
+#define GREENTEA_PHP_H
+    #define GREENTEA_PHP_H "0.0.1"
+
+    #include "php.h"
+
+    #ifdef HAVE_CONFIG_H
+        #include "config.h"
+    #endif
+
+    #ifdef ZTS
+        #include "TSRM.h"
+    #endif
+
+    PHP_INI_BEGIN()
+    PHP_INI_END()
+
+    #ifndef GREENTEA_PHP_MAIN
+        extern zend_module_entry greentea_module_entry;
+    #endif
+
+    ZEND_BEGIN_MODULE_GLOBALS(greentea)
+    ZEND_END_MODULE_GLOBALS(greentea)
+    ZEND_EXTERN_MODULE_GLOBALS(greentea)
+    #define GREENTEAG(v) ZEND_MODULE_GLOBALS_ACCESSOR(greentea, v)
+
+
+    #if defined(ZTS) && defined(COMPILE_DL_SAMPLE)
+        ZEND_TSRMLS_CACHE_EXTERN()
+    #endif
+
+    #define phpext_greentea_ptr (&greentea_module_entry)
+#endif
