e957464e265758fbd4a47673b278ce0971cd583b
Ammar Faizi <ammarfaizi2@gmail.com>
2020-04-24 17:24:28 +0700

    Clean up

--- src/build_scripts/greentea_php/builder.php
+++ src/build_scripts/greentea_php/builder.php
@@ -12,11 +12,6 @@ if (!file_exists($buildDir)) {
     );
 }
 
-she("cp -asvf ".
-    escapeshellarg(APP_DIR)." ".
-    escapeshellarg($buildDir)
-);
-
 ConfigM4::addFile("greentea_php.c");
 
 // Scan all greentea C files.
@@ -48,3 +43,29 @@ recursive_callback_scan(
 PHPClass::compile(GREENTEA_PHP_SRC_DIR."/greentea_php.php.c", $buildDir, "greentea.c");
 ConfigM4::addIncludePath(GREENTEA_PHP_SRC_DIR);
 ConfigM4::buildConfigM4File($configM4File, $buildDir);
+
+she("cp -asvf ".
+    escapeshellarg(APP_DIR)." ".
+    escapeshellarg($buildDir));
+
+// Scan all greentea C files.
+recursive_callback_scan(
+    APP_DIR,
+    function (string $file, string $dir, int $index) use (&$cFiles, $buildDir) {
+        if ($file === "greentea_php.php.c") {
+            return;
+        }
+
+        $edir = explode(GREENTEA_PHP_SRC_DIR, $dir, 2);
+        $edir = isset($edir[0]) ? rtrim($edir[1], "/")."/" : "";
+
+        if (preg_match("/(.+)\.php\.c$/", $file, $m)) {
+            $targetDir = $buildDir."/app/".$edir;
+            $targetFile = $m[1].".compiled.php";
+            is_dir($buildDir) or mkdir($buildDir);
+            is_dir($targetDir) or mkdir($targetDir);
+
+            PHPClass::compile($dir."/".$file, $targetDir, $targetFile);
+        }
+    }
+);
