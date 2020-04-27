060e68cd99da575c80bb633c3d382a2fc6e06785
Ammar Faizi <ammarfaizi2@gmail.com>
2020-04-27 11:16:51 +0700

    Prepare database and config

---
+++ config.php.hpp
@@ -0,0 +1,15 @@
+
+#ifndef GREENTEA_CONFIG_HPP
+#define GREENTEA_CONFIG_HPP
+
+#define APP_URL "http://127.0.0.1"
+#define ASSET_URL APP_URL"/assets"
+
+// MySQL Database.
+#define DB_HOST "192.168.50.1"
+#define DB_PORT 3306
+#define DB_USER "memcpy"
+#define DB_PASS "123qweASDzxc"
+#define DB_NAME "greentea"
+
+#endif
